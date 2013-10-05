e4bfd2aefa552b95dde984402200fd6af9006402
Yar Rick <yar@yareg.com>
2013-10-05 16:24:27 +0400

    fix app name

--- tools/BluetoothSCOApp/Android.mk
+++ tools/BluetoothSCOApp/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := BluetoothSCOApp
+LOCAL_PACKAGE_NAME := BluetoothMusic
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := shared
 
